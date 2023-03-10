bf51362f45359a0524dcdea55d221360e0d464ab
abhibhalani <abhibhalanidrop@gmail.com>
2023-03-10 15:50:58 +0530

    update file

--- src/ToDolist.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ToDoList(props) {
-    return (
-        <>
-            <li> <i className="fas fa-times-circle" onClick={(() => { props.onSelect(props.id) })}></i>{props.text}</li>
-        </>
-    )
-}
